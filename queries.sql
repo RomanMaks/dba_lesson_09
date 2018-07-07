f13f6734e461fc81d5831ee5e2e0cbecae4fedf8
Maksimov Roman <maksimov.ro.al@gmail.com>
2018-07-07 08:28:41 +0300

    Сделано 3 задание

--- queries.sql
+++ queries.sql
@@ -3,45 +3,54 @@
 --    вес (weight) и так далее, что вам придет в голову. Заполните
 --    это поле различными характеристиками в виде json.
 
-  ALTER TABLE products ADD characteristic jsonb NULL;
+  ALTER TABLE products ADD properties jsonb NULL;
 
   update products
-  SET characteristic = '{"color": "red", "size": "40", "gender": "female"}'
+  SET properties = '{"color": "red", "size": "40", "gender": "female"}'
   WHERE id = 1 -- Кеды
 
   update products
-  SET characteristic = '{"color": "green", "weight": "400"}'
+  SET properties = '{"color": "green", "weight": "400"}'
   WHERE id = 5 -- Кружка
 
   update products
-  SET characteristic = '{"color": "blue", "size": "XXL", "gender": "male"}'
+  SET properties = '{"color": "blue", "size": "XXL", "gender": "male"}'
   WHERE id = 14 -- Джинсы
 
   update products
-  SET characteristic = '{"color": "red", "size": "XXL", "weight": "1300", "gender": "male"}'
+  SET properties = '{"color": "red", "size": "XXL", "weight": "1300", "gender": "male"}'
   WHERE id = 11 -- Куртка
 
 -- 2. Найдите товары:
 
   -- 1. У которых есть характеристика цвет, но нет размера
-    SELECT products.characteristic
+    SELECT *
     FROM products
-    WHERE characteristic ? 'color'
-      AND NOT characteristic ? 'size'
+    WHERE properties ? 'color'
+      AND NOT properties ? 'size'
 
   -- 2. У которых вес не более килограмма
     SELECT *
     FROM products
-    WHERE (characteristic->>'weight')::integer <= 1000
+    WHERE (properties->>'weight')::integer <= 1000
 
   -- 3. Красного цвета и размера XXL
     SELECT *
     FROM products
-    WHERE characteristic->>'color' = 'red' 
-      AND characteristic->>'size' = 'XXL'
+    WHERE properties->>'color' = 'red' 
+      AND properties->>'size' = 'XXL'
 
 -- 3. Создайте материализированное представление, которое поля jsonb
 --    превратит в столбцы (color, size, weight)
+  CREATE MATERIALIZED VIEW m_properties AS
+    SELECT
+      vendor_code,
+      name,
+      properties->>'color' AS color,
+      properties->>'size' AS size,
+      properties->>'weight' AS weight,
+      properties->>'gender' AS gender
+    FROM products;
 
 -- 4. * Используя оконные функции напишите запрос, который вернет все
 --    товары и для каждого - его долю в процентах в общей стоимости
